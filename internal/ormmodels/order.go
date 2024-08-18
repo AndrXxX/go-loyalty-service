0796e30e714a5b902a227f1a2e8b04ce4f148935
Andrew Garetov <andrew1gar@yandex.ru>
2024-08-18 22:32:30 +0330

    #1 В модели добавлены доп. поля

--- internal/ormmodels/order.go
+++ internal/ormmodels/order.go
@@ -1,11 +1,17 @@
 package ormmodels
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Order struct {
 	gorm.Model
-	Number  string `gorm:"uniqueIndex"`
-	Status  string
-	Accrual *float64
-	Author  User `gorm:"embedded"`
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Number    string `gorm:"uniqueIndex"`
+	Status    string
+	Accrual   *float64
+	Author    User `gorm:"embedded"`
 }
--- internal/ormmodels/user.go
+++ internal/ormmodels/user.go
@@ -1,9 +1,15 @@
 package ormmodels
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
 	gorm.Model
-	Login    string `gorm:"uniqueIndex"`
-	Password string
+	ID        uint `gorm:"primarykey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Login     string `gorm:"uniqueIndex"`
+	Password  string
 }
