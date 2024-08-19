623692c028defc6177921736397b51fef85f1488
Andrew Garetov <andrew1gar@yandex.ru>
2024-08-19 11:25:18 +0330

    #1 Реализация endpoint Withdrawals

--- internal/controllers/balance_controller.go
+++ internal/controllers/balance_controller.go
@@ -1,9 +1,14 @@
 package controllers
 
 import (
+	"encoding/json"
 	"github.com/AndrXxX/go-loyalty-service/internal/entities"
+	"github.com/AndrXxX/go-loyalty-service/internal/enums"
+	"github.com/AndrXxX/go-loyalty-service/internal/enums/contenttypes"
 	"github.com/AndrXxX/go-loyalty-service/internal/interfaces"
 	"github.com/AndrXxX/go-loyalty-service/internal/ormmodels"
+	"github.com/AndrXxX/go-loyalty-service/internal/services/logger"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -35,6 +40,28 @@ func (c *balanceController) Withdraw(_ http.ResponseWriter, _ *http.Request) {
 	// TODO
 }
 
-func (c *balanceController) Withdrawals(_ http.ResponseWriter, _ *http.Request) {
-	// TODO
+func (c *balanceController) Withdrawals(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value(enums.UserId).(uint)
+	user := c.us.Find(&ormmodels.User{ID: userId})
+	if user == nil {
+		logger.Log.Error("failed to find user", zap.Uint("userId", userId))
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	withdraws := c.ws.FindAll(&ormmodels.Withdraw{Author: *user})
+	list := c.wc.ConvertMany(withdraws)
+	encoded, err := json.Marshal(list)
+	if err != nil {
+		logger.Log.Error("failed to encode withdraws list", zap.Error(err), zap.Uint("userId", userId))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(encoded)
+	if err != nil {
+		logger.Log.Error("failed to write withdraws list response", zap.Error(err), zap.Uint("userId", userId))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", contenttypes.ApplicationJSON)
+	w.WriteHeader(http.StatusOK)
 }
