a2d9e5d31d95f62a6f3e832594654dabf63f4a97
Andrew Garetov <andrew1gar@yandex.ru>
2024-08-21 11:27:32 +0300

    #1 Расширение покрытия тестами env

--- cmd/gophermart/env_test.go
+++ cmd/gophermart/env_test.go
@@ -15,6 +15,12 @@ func Test_parseEnv(t *testing.T) {
 		want   *config.Config
 	}{
 		{
+			name:   "Incorrect environment variables",
+			config: &config.Config{},
+			env:    map[string]string{"AUTH_SECRET_KEY_EXPIRED": "abc"},
+			want:   &config.Config{},
+		},
+		{
 			name:   "Empty env",
 			config: &config.Config{RunAddress: "host"},
 			env:    map[string]string{},
@@ -31,6 +37,12 @@ func Test_parseEnv(t *testing.T) {
 			config: &config.Config{},
 			env:    map[string]string{"RUN_ADDRESS": "new-host", "DATABASE_URI": "test", "ACCRUAL_SYSTEM_ADDRESS": "test2"},
 			want:   &config.Config{RunAddress: "new-host", DatabaseURI: "test", AccrualSystemAddress: "test2"},
+		},
+		{
+			name:   "AUTH_SECRET_KEY=test1 AUTH_SECRET_KEY_EXPIRED=1 PASSWORD_SECRET_KEY=test2",
+			config: &config.Config{},
+			env:    map[string]string{"AUTH_SECRET_KEY": "test1", "AUTH_SECRET_KEY_EXPIRED": "1", "PASSWORD_SECRET_KEY": "test2"},
+			want:   &config.Config{AuthKey: "test1", AuthKeyExpired: 1, PasswordKey: "test2"},
 		},
 	}
 	for _, tt := range tests {
