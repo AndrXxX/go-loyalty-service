8acd91f3b814207273fb72e1348ac156149bd363
Andrew Garetov <andrew1gar@yandex.ru>
2024-08-21 11:15:56 +0300

    #1 Упрощение кода: использование generic для реализации одного хранилища

--- internal/storages/factory.go
+++ internal/storages/factory.go
@@ -2,6 +2,7 @@ package storages
 
 import (
 	"context"
+	"github.com/AndrXxX/go-loyalty-service/internal/ormmodels"
 	"github.com/AndrXxX/go-loyalty-service/internal/services/logger"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -15,29 +16,23 @@ func Factory(db *gorm.DB) *storagesFactory {
 	return &storagesFactory{db}
 }
 
-func (f *storagesFactory) UserStorage(ctx context.Context) *userStorage {
-	us := &userStorage{f.db}
-	err := us.init(ctx)
-	if err != nil {
-		logger.Log.Error("failed to Init userStorage", zap.Error(err))
-	}
-	return us
+func (f *storagesFactory) UserStorage(ctx context.Context) *ormStorage[ormmodels.User] {
+	return getStorage[ormmodels.User](ctx, f.db, new(ormmodels.User))
 }
 
-func (f *storagesFactory) OrderStorage(ctx context.Context) *orderStorage {
-	s := &orderStorage{f.db}
-	err := s.init(ctx)
-	if err != nil {
-		logger.Log.Error("failed to Init orderStorage", zap.Error(err))
-	}
-	return s
+func (f *storagesFactory) OrderStorage(ctx context.Context) *ormStorage[ormmodels.Order] {
+	return getStorage[ormmodels.Order](ctx, f.db, new(ormmodels.Order))
 }
 
-func (f *storagesFactory) WithdrawStorage(ctx context.Context) *withdrawStorage {
-	s := &withdrawStorage{f.db}
-	err := s.init(ctx)
+func (f *storagesFactory) WithdrawStorage(ctx context.Context) *ormStorage[ormmodels.Withdraw] {
+	return getStorage[ormmodels.Withdraw](ctx, f.db, new(ormmodels.Withdraw))
+}
+
+func getStorage[T interface{}](ctx context.Context, db *gorm.DB, m *T) *ormStorage[T] {
+	s := &ormStorage[T]{db}
+	err := s.init(ctx, m)
 	if err != nil {
-		logger.Log.Error("failed to Init withdrawStorage", zap.Error(err))
+		logger.Log.Error("failed to init storage", zap.Error(err))
 	}
 	return s
 }
